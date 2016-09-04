ed1c78bad91494a9c1c4f5aa14ce17bfe485a25a
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-09-04 18:05:31 +0100

    Add lasp_pull_s3_logs.sh

---
+++ lasp_pull_s3_logs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo ">>> AWS_ACCESS_KEY_ID is not configured; please export it."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo ">>> AWS_SECRET_ACCESS_KEY is not configured; please export it."
+  exit 1
+fi
+
+## Configure AWS CLI
+##   - see how to install it here: 
+##     http://docs.aws.amazon.com/cli/latest/userguide/installing.html
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
+## Sync s3 bucket with local logs folder
+aws s3 sync s3://lasp-transmission-instrumentation-logs logs/
